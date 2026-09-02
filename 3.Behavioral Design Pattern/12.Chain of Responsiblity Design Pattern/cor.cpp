d0c40d7318b57d38216e4df6a0b93a4d03065779
Rocky0412 <rockytapader4g@gmail.com>
2026-09-02 23:57:29 +0530

    Chain of Responsiblity added

--- 3.Behavioral Design Pattern/12.Chain of Responsiblity Design Pattern/cor.cpp
+++ 3.Behavioral Design Pattern/12.Chain of Responsiblity Design Pattern/cor.cpp
@@ -3,3 +3,50 @@
 #include <vector>
 
 using namespace std;
+class IMoneyHandaler{
+    
+    public:
+    IMoneyHandaler* nextHandaler;
+    IMoneyHandaler(){
+        this->nextHandaler=nullptr;
+    }
+    void setNextHandler(IMoneyHandaler* next){
+        this->nextHandaler=next;
+    }
+    virtual void dispense(int amount)=0;
+};
+class FiveHundred: public IMoneyHandaler{
+    public:
+
+    void dispense(int amount){
+        int count= amount/500;
+        if(count>0){
+            amount=amount - count * 500;
+            cout<< "No. 500 notes is : "<< count<<endl;
+        }
+        nextHandaler->dispense(amount);
+
+    }
+};
+class OneHundred: public IMoneyHandaler{
+    public:
+
+    void dispense(int amount){
+        int count= amount/100;
+        if(count>0){
+            amount=amount - count * 100;
+            cout<< "No. 100 notes is : "<< count<<endl;
+        }
+        cout<<"Remaining amount : "<<amount<<endl;
+      
+    }
+};
+
+int main(){
+    IMoneyHandaler* five= new FiveHundred();
+    IMoneyHandaler* one= new OneHundred();
+    five->setNextHandler(one);
+    five->dispense(5420);
+
+}
+
